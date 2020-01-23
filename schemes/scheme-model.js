66125b08c4bb08c45602cd06e01ed236bf0a9b36
Victor Tran <victor.n.tran@outlook.com>
2020-01-23 18:21:25 -0500

    format doc

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -15,11 +15,23 @@ const findSteps = id => {
   // find steps
   // belonging to a specific scheme
   // then order by step number
-  return (db('steps').join('schemes', {'schemes.id': 'steps.scheme_id'})).select('steps.id','scheme_name', 'step_number', 'instructions').where({'schemes.id': id}).orderBy('steps.step_number');
+  return db("steps")
+    .join("schemes", { "schemes.id": "steps.scheme_id" })
+    .select("steps.id", "scheme_name", "step_number", "instructions")
+    .where({ "schemes.id": id })
+    .orderBy("steps.step_number");
 };
 
+const add = scheme => {
+  return db("schemes")
+    .insert(scheme)
+    .then(ids => {
+      return findById(ids[0]);
+    });
+};
 module.exports = {
   find,
   findById,
-  findSteps
+  findSteps,
+  add
 };
