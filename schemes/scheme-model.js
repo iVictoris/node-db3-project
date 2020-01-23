59ae57d9310e9b927ea760df7973c092e4f8fac4
Victor Tran <victor.n.tran@outlook.com>
2020-01-23 17:56:19 -0500

    create helper functions find, findById, findSteps

---
+++ schemes/scheme-model.js
@@ -0,0 +1,25 @@
+const dbConfig = require("../knexfile");
+const knex = require("knex");
+
+const db = knex(dbConfig.development);
+
+const find = () => {
+  return db("schemes");
+};
+
+const findById = id => {
+  return db("schemes").where({ id });
+};
+
+const findSteps = id => {
+  // find steps
+  // belonging to a specific scheme
+  // then order by step number
+  return (db('steps').join('schemes', {'schemes.id': 'steps.scheme_id'})).select('steps.id','scheme_name', 'step_number', 'instructions').where({'schemes.id': id}).orderBy('steps.step_number');
+};
+
+module.exports = {
+  find,
+  findById,
+  findSteps
+};
